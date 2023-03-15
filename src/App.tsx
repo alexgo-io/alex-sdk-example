60ecf9e2bda74c2afa4909de2482a7e71534d714
Kyle Fang <zhigang1992@gmail.com>
2023-03-15 14:14:32 +0800

    feat: add basic example of alexsdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+
+import {
+  AppConfig,
+  openContractCall,
+  showConnect,
+  UserData,
+  UserSession,
+} from "@stacks/connect";
+import { AlexSDK, Currency } from "alex-sdk";
+
+const appConfig = new AppConfig([]);
+const userSession = new UserSession({ appConfig });
+const appDetails = {
+  name: "Alex Swap Example",
+  icon: "https://cdn.alexlab.co/logos/ALEX_Token.png",
+};
+
+const alex = new AlexSDK();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState<UserData | undefined>(undefined);
+
+  useEffect(() => {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        setUserData(userData);
+      });
+    } else if (userSession.isUserSignedIn()) {
+      setUserData(userSession.loadUserData());
+    }
+  }, []);
+
+  const onConnectWallet = useCallback(() => {
+    showConnect({
+      appDetails,
+      onFinish: () => window.location.reload(),
+      userSession,
+    });
+  }, []);
+
+  const [from, setFrom] = useState<Currency>(Currency.STX);
+  const [amount, setAmount] = useState("");
+  const [to, setTo] = useState<Currency>(Currency.ALEX);
+
+  const valid =
+    from != null &&
+    to != null &&
+    amount !== "" &&
+    !isNaN(Number(amount)) &&
+    from !== to;
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+          marginBottom: 10,
+        }}
+      >
+        <p>From:</p>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="amount"
+        />
+        <select
+          value={from}
+          onChange={(e) => setFrom(e.target.value as Currency)}
+        >
+          {Object.values(Currency).map((a) => (
+            <option key={a} value={a}>
+              {a}
+            </option>
+          ))}
+        </select>
+        <p>to:</p>
+        <select value={to} onChange={(e) => setTo(e.target.value as Currency)}>
+          {Object.values(Currency).map((a) => (
+            <option key={a} value={a}>
+              {a}
+            </option>
+          ))}
+        </select>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {userData == null ? (
+        <button onClick={onConnectWallet}>Connect Wallet</button>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+          <button
+            disabled={!valid}
+            onClick={async () => {
+              const result = await alex.getFee(from!, to!);
+              const feeRate = Number(result) / 1e8;
+              alert(
+                `Fee rate is ${feeRate * 100}%, fee is ${
+                  Number(amount) * feeRate
+                } ${from}`
+              );
+            }}
+          >
+            Get Fee
+          </button>
+          <button
+            disabled={!valid}
+            onClick={async () => {
+              const result = await alex.getAmountTo(
+                from!,
+                BigInt(Number(amount) * 1e8),
+                to!
+              );
+              alert(`You will get ${Number(result) / 1e8} ${to}`);
+            }}
+          >
+            Get Rate
+          </button>
+          <button
+            disabled={!valid}
+            onClick={async () => {
+              const routers = await alex.getRouter(from!, to!);
+              alert(`Routers: ${routers.join(" -> ")}`);
+            }}
+          >
+            Get Route
+          </button>
+          <button
+            disabled={!valid}
+            onClick={async () => {
+              const stxAddress = userData!.profile.stxAddress.mainnet;
+              const routers = await alex.getRouter(from!, to!);
+              const tx = await alex.runSwap(
+                stxAddress,
+                from!,
+                to!,
+                BigInt(Number(amount) * 1e8),
+                BigInt(0),
+                routers
+              );
+              await openContractCall(tx);
+            }}
+          >
+            Swap
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
